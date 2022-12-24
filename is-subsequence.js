4f0c1847151b2eb312b7edd15b227b96dacf620e
ollie <olivinanzali07@gmail.com>
2022-12-23 18:13:27 -0600

    added isSubsequence code

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,27 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(string1, string2) {
+    if(!string1 || !string2){
+        return fales;
+    }
+
+    stringArray2 = [];
+    string1Object = {};
+    
+    for(char in string1){
+        if(char !== string1[char]){
+            string1Object[char]++;
+        } else{
+            string1Object[char]+=1;
+        }
+    for(char in string2){
+        if(char === string1Object[char]) {
+            string1Object[char]-=1;
+            if(string1Object[char] === 0){
+                delete string1Object[char];
+                stringArray2.push(char);
+            }
+        }
+        }
+        return string1 === stringArray2.join("")
+    }
 }
